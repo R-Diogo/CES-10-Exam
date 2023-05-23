34bfdd4dbff90f346e2f2dafd51d9ef7258d5249
catatau <dibuenor@gmail.com>
2023-05-23 02:16:27 -0300

    add structure

--- main.cpp
+++ main.cpp
@@ -8,95 +8,138 @@ enum class Job {
     Going,
     Strenuous,
     Median,
+    Professor,
+    Potion, //tem q implementar q so tem uma quantidade fixa de poções
 };
 
-struct Stats {
-    int poder_gaga;
-    int mela_aula;
-};
-
-enum class Status {
+enum class Status{
     Smooth,
     Tired,
+    Dizzy,
 };
 
-enum class Attacks : int{
-    got_nothing = 11,
-    what_times_ends = 12,
-    help_me = 13,
-    want_comp = 21,
-    full_nightlong = 22,
-    always_went_for_ELE = 31,
-    gonna_quit = 32,
-    gonna_see_the_norm = 41,
-    hole_your_xxx = 42,
-    disconnect_ITA = 43,
-    forgot_key = 51,
-    friday_no_office_hour = 52,
-    test_worth_12 = 53,
-    theory_important = 61,
-    test_easy = 62,
-    no_need_calculate = 63,
+enum class Movement{
+    comp_comp,
+    full_nightlong,
+    tired_insane,
+    got_nothing,
+    when_end,
+    help_here,
+    always_ELE,
+    gonna_quit,
+    see_norm,
+    hole_your_xxx,
+    disconnect_ITA,
+    forgot_key,
+    friday_no_office_hour,
+    test_worth_12,
+    theory_important,
+    test_easy,
+    no_need_calculate ,
 };
 
 struct Character {
     std::string name;
     Job job;
-    Stats stats;
     Status status;
-    Attacks attacks[3];
+    Movement moves[3];
+    int tired;
 };
 
-void attack_move(Attacks _attack){
+Character load_character(std::fstream&);
+
+void attack_move(Movement);
+
+void make_move(Character);
+
+void ai_attack(Character, Character, Character, Character);
+
+void fight(Character, Character, Character, Character, bool);
+
+int main(){
+    //mano eu demorei um seculo para achar q tinha q por o struct no começo (mad :X)
+    struct Character catatau = {
+        "Catatau", Job::Strenuous, Status::Smooth,{Movement::comp_comp,Movement::full_nightlong, Movement::tired_insane}, 0};
+    struct Character catabol = {
+        "Catabol", Job::Median, Status::Smooth,{}, 0};
+    struct Character gripen = {
+        "Gripen", Job::Going, Status::Smooth,{Movement::gonna_quit}, 0};
+    struct Character yu = {
+        "Yu", Job::Going, Status::Smooth,{Movement::see_norm, Movement::hole_your_xxx, Movement::disconnect_ITA}, 0};
+    struct Character nobili = {
+        "Nobili", Job::Strenuous, Status::Smooth,{Movement::theory_important, Movement::test_easy, Movement::no_need_calculate}, 0};
+    struct Character jenlo = {
+        "Jennifer Lopes", Job::Strenuous, Status::Smooth,{Movement::see_norm, Movement::test_easy}, 0};
+    struct Character verri = {
+        "Verri", Job::Median, Status::Smooth,{Movement::forgot_key, Movement::friday_no_office_hour, Movement::test_worth_12}, 0};
+    struct Character fritz_rene = {
+        "Fritz and Rene", Job::Potion, Status::Smooth,{}, 0};
+    std::array<Character,9> all_characters={
+        gripen, catatau, catabol, yu, nobili, jenlo, verri, fritz_rene};
+    std::string file_name;
+    std::cout<<"Enter a valid file with your current safe stage (if you don't have one enter None):\n";
+    std::cin>>file_name;
+    std::fstream file(file_name, std::ios::in);
+    //eu to em duvida ainda com usa o file na função, tipo se toda vez que eu chamo a função ele reabre ou continua da onde parou
+    for(int i=0;i<9;i++){
+        all_characters[i]=load_character(file);
+    }
+    bool professor_turn; //tem que pegar isso do arquivo tambem
+    if(yu.status != Status::Tired){
+        fight(yu, catatau, catabol, gripen, professor_turn);
+    }
+    if(nobili.status != Status::Tired){
+        fight(nobili, catatau, catabol, gripen, professor_turn);
+    }
+    if(jenlo.status != Status::Tired){
+        fight(jenlo, catatau, catabol, gripen, professor_turn);
+    }
+    return 0;
+}
+
+Character load_character(std::fstream& _file){
+    //minha ideia é colocar no arquivo tipo:
+    //Catatau Status(ai tem q add os status sonolento etc) vida
+    //ai com base nos professores que a gente leu a gente ja sabe o stage que a gente ta
+    //tipo se um professor morreu a gente n ta nesse stage 
+}
+void fight(Character _professor, Character _catatau, Character _catabol, Character _gripen, bool _professor_turn){
+    while(true){
+        if(_professor_turn){
+            ai_attack(_professor, _catatau, _catabol, _gripen);
+            //ta sugou, ainda tenho q pensar como implementar, pq a função tem que alterar a vida de mais de um player
+        }
+        else{
+            if(_catatau.status==Status::Smooth){
+                make_move(_catatau);
+                //acho q eu vou mudar para retornar o dano q deu no professor
+            }
+            //... para os outros
+        }
+    }
+}
+
+void attack_move(Movement _attack){
     switch(_attack){
-        case Attacks::got_nothing:
+        case Movement::got_nothing:
             //fazer o que faz
         //add os outros
     }
 }
-
 void make_move(Character _character){
     int attack_choice;
     //implementar bagui pra ver as opções
     std::cin>>attack_choice;
     switch (attack_choice){
         case 1:
-            attack_move(_character.attacks[0]);
+            attack_move(_character.moves[0]);
             break;
         case 2:
-            attack_move(_character.attacks[1]);
+            attack_move(_character.moves[1]);
             break;
         case 3:
-            attack_move(_character.attacks[2]);
+            attack_move(_character.moves[2]);
             break;
         //adicionar mais coisas tipo sair usar poção etc (mais pro final)
     }
-}
-//implementar quem começa
-void first_fight(Character _verri, Character _gripen, Character _catatau, Character _catabol){
-    while(true){
-
-    }
-}
-
-int main(){
-    Character gripen = {
-        "Gripen", Job::Going, {100, 10}, Status::Smooth,{Attacks::always_went_for_ELE, Attacks::gonna_quit}};
-
-    Character catatau = {
-        "Catatau", Job::Strenuous, {10, 100}, Status::Smooth,{Attacks::want_comp,Attacks::full_nightlong}};
-
-    Character catabol = {
-        "Catabol", Job::Median, {10, 10}, Status::Smooth,{Attacks::got_nothing, Attacks::what_times_ends, Attacks::help_me}};
-
-    Character yu = {
-        "Yu", Job::Going, {80, 20}, Status::Smooth,{Attacks::gonna_see_the_norm, Attacks::hole_your_xxx, Attacks::disconnect_ITA}};
-
-    Character verri = {
-        "Verri", Job::Median, {70, 30}, Status::Smooth,{Attacks::forgot_key, Attacks::friday_no_office_hour, Attacks::test_worth_12}};
-
-    Character nobili = {
-        "Nobili", Job::Strenuous, {40, 60}, Status::Smooth,{Attacks::theory_important, Attacks::test_easy, Attacks::no_need_calculate}};
-    
-    return 0;
 }
