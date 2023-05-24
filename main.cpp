6b1e0859b7288fd9f276e7fde970ffece93e2456
catatau <dibuenor@gmail.com>
2023-05-24 09:13:46 -0300

    remove pointers

--- main.cpp
+++ main.cpp
@@ -59,51 +59,43 @@ struct Potion {
     int num_potions[2];
 };
 
-void load_character(std::fstream&, Character*);
+void load_character(std::fstream&, Character[]);
 
-void load_potion(std::fstream&, Potion*);
+void load_potion(std::fstream&, Potion&);
 
 Status string_status(std::string);
 
 void display(Character);
 
-void attack_move(Movement, int*, int*);
+void attack_move(Movement, int&, int&);
 
-void make_move(Character, int*, int*);
+void make_move(Character, int&, int&);
 
 void ai_attack(Character, Character, Character, Character);
 
 void fight(Character, Character, Character, Character, bool);
 
 int main(){
-    Character catatau, catabol, gripen, yu, nobili, lopes;
-    struct Potion verri = {
-        "Verri", {Potion_Type::no_key, Potion_Type::friday_no_class}, {5, 5}};
-    struct Potion fritz_rene = {
-        "Fritz_Rene", {Potion_Type::renefritz_1potion, Potion_Type::renefritz_2potion}, {5, 5}};
+    Character characters[6];
+    Potion verri, fritz_rene;
     std::string file_name;
     std::cout<<"Enter a valid file with your current safe stage (if you don't have one enter one you would like to create):\n";
     std::cin>>file_name;
     std::fstream stage(file_name, std::ios::in);
-    load_character(stage, &catatau);
-    load_character(stage, &catabol);
-    load_character(stage, &gripen);
-    load_character(stage, &yu);
-    load_character(stage, &nobili);
-    load_character(stage, &lopes);
-    load_potion(stage, &verri);
-    load_potion(stage, &fritz_rene);
+    load_character(stage, characters);
+    load_potion(stage, verri);
+    load_potion(stage, fritz_rene);
     //ta funcionando o load_character mas tem q deixar melhor eu acho
     //tambem tem q fazer funcionar se n tiver nenhum arquivo previamente salvo
     bool professor_turn=false;
-    if(yu.status != Status::Tired){
-        fight(yu, catatau, catabol, gripen, professor_turn);
+    if(characters[3].status != Status::Tired){
+        fight(characters[3], characters[0], characters[1], characters[2], professor_turn);
     }
-    if(nobili.status != Status::Tired){
-        fight(nobili, catatau, catabol, gripen, professor_turn);
+    if(characters[4].status != Status::Tired){
+        fight(characters[4], characters[0], characters[1], characters[2], professor_turn);
     }
-    if(lopes.status != Status::Tired){
-        fight(lopes, catatau, catabol, gripen, professor_turn);
+    if(characters[5].status != Status::Tired){
+        fight(characters[5], characters[0], characters[1], characters[2], professor_turn);
     }
     return 0;
 }
@@ -114,46 +106,48 @@ Status string_status(std::string _stats){
     else if(_stats=="Dizzy") return Status::Dizzy;
 }
 
-void load_character(std::fstream& _stage, Character *_character){
+void load_character(std::fstream& _stage, Character& _character){
     std::string _name, _stats;
     int _tired;
-    _stage>>_name>>_stats>>_tired;
-    Status _status = string_status(_stats);
-    if(_name=="Catatau"){
-        *_character = {
+    for(int i=0;i<6;i++){
+        _stage>>_name>>_stats>>_tired;
+        Status _status = string_status(_stats);
+        if(_name=="Catatau"){
+            _character = {
             "Catatau", Job::Strenuous, _status, {Movement::comp_comp, Movement::full_nightlong, Movement::tired_insane}, _tired};
-    }
-    else if(_name=="Catabol"){
-        *_character ={
+        }
+        else if(_name=="Catabol"){
+            _character ={
             "Catabol", Job::Going, _status,{Movement::got_nothing, Movement::when_end, Movement::help_here}, _tired};  
-    }
-    else if(_name=="Gripen"){
-        *_character={
+        }
+        else if(_name=="Gripen"){
+            _character={
             "Gripen", Job::Median, _status,{Movement::gonna_quit, Movement::always_ELE, Movement::gripen_3attack}, _tired};
-    }
-    else if(_name=="Yu"){
-        *_character={
+        }
+        else if(_name=="Yu"){
+            _character={
             "Yu", Job::Professor, _status,{Movement::hole_your_xxx, Movement::see_norm, Movement::disconnect_ITA}, _tired};
-    }
-    else if(_name=="Nobili"){
-        *_character={
+        }
+        else if(_name=="Nobili"){
+            _character={
             "Nobili", Job::Professor, _status,{Movement::no_class, Movement::counter_example, Movement::nobili_3attack}, _tired};
-    }
-    else{
-        *_character={
+        }
+        else{
+            _character={
             "Lopes", Job::Professor, _status,{Movement::lopes_1attack, Movement::lopes_2attack, Movement::lopes_3attack}, _tired};
+        }
     }
 }
 
-void load_potion(std::fstream& _stage, Potion *_potion){
+void load_potion(std::fstream& _stage, Potion& _potion){
     std::string _potion_name;
     int num_potion1, num_potion2;
     _stage>>_potion_name>>num_potion1>>num_potion2;
     if(_potion_name=="Verri"){
-        *_potion={"Verri", {Potion_Type::no_key, Potion_Type::friday_no_class}, {num_potion1, num_potion2}};
+        _potion={"Verri", {Potion_Type::no_key, Potion_Type::friday_no_class}, {num_potion1, num_potion2}};
     }
     else{
-        *_potion={"Fritz_Rene",{Potion_Type::renefritz_1potion, Potion_Type::renefritz_2potion}, {num_potion1, num_potion2}};
+        _potion={"Fritz_Rene",{Potion_Type::renefritz_1potion, Potion_Type::renefritz_2potion}, {num_potion1, num_potion2}};
     }
 }
 
@@ -167,18 +161,18 @@ void fight(Character _professor, Character _catatau, Character _catabol, Charact
         else{
             int make_tired=0, get_tired=0;
             if(_catatau.status==Status::Smooth){
-                make_move(_catatau, &make_tired, &get_tired);
+                make_move(_catatau, make_tired, get_tired);
                 _professor.tired+=make_tired;
                 _catatau.tired+=get_tired;
                 std::cout<<"Yu is "<<_professor.tired<<" tired and Catatau is "<<_catatau.tired<<" tired\n";
             }
             if(_catabol.status==Status::Smooth){
-                make_move(_catabol, &make_tired, &get_tired);
+                make_move(_catabol, make_tired, get_tired);
                 _professor.tired+=make_tired;
                 _catabol.tired+=get_tired;
             }
             if(_gripen.status==Status::Smooth){
-                make_move(_gripen, &make_tired, &get_tired);
+                make_move(_gripen, make_tired, get_tired);
                 _professor.tired+=make_tired;
                 _catabol.tired+=get_tired;
             }
@@ -187,23 +181,23 @@ void fight(Character _professor, Character _catatau, Character _catabol, Charact
     }
 }
 
-void attack_move(Movement _attack, int *_make, int *_get){
+void attack_move(Movement _attack, int& _make, int& _get){
     switch(_attack){
         case Movement::comp_comp:
-            *_make=15;
-            *_get=5;
+            _make=15;
+            _get=5;
             break;
         case Movement::full_nightlong:
-            *_make=25;
-            *_get=30;
+            _make=25;
+            _get=30;
             break;
         case Movement::tired_insane:
-            *_get=-30;
+            _get=-30;
             break;
         //add os outros
     }
 }
-void make_move(Character _character, int *make, int *get){
+void make_move(Character _character, int& make, int& get){
     int attack_choice;
     display(_character);
     std::cin>>attack_choice;
